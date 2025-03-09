24a75dbd9c36c7b033887bdd9f5189b8cd9a9d91
Mathew Lopez <mathewlopez10@gmail.com>
2025-03-09 17:55:30 -0400

    Removed logging of MONGOURI

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -2,13 +2,12 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks'
+const MONGODB_URI = process.env.MONGODB_URI || ''
 
 const db = async () => {
     try {
         await mongoose.connect(MONGODB_URI);
         console.log('Database connected.');
-        console.log(MONGODB_URI)
         return mongoose.connection;
     } 
     catch (err) {
